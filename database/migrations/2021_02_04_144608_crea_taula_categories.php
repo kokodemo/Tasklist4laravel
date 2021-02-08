70d3b15566629176c1e43dd635faeb7ede651336
kokodemo <xnominatorxx@gmail.com>
2021-02-08 19:02:29 +0100

    finish

---
+++ database/migrations/2021_02_04_144608_crea_taula_categories.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreaTaulaCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('parent')->nullable();
+            $table->foreign('parent')->references('id')->on('categories') ;
+
+            $table->timestamps();
+        });
+
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedBigInteger('cat_id')->nullable();
+            $table->foreign('cat_id')->references('id')->on('categories');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::dropIfExists('categories');
+    }
+}
